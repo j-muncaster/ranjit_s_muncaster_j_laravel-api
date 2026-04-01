1b1ab09930cf79bcffef9ce4b03e341fc5ff4ea9
Jo Muncaster <j_muncaster208947@fanshaweonline.ca>
2026-04-01 14:23:24 -0400

    Create: made the factories for toys and brands

---
+++ backend/database/factories/ToyFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Toy;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Toy>
+ */
+class ToyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'toy_name' => fake()->unique()->words(2, true),
+            'toy_description' => fake()->unique()->paragraph(),
+            'toy_brand' => fake()->unique()->words(2, true),
+            'toy_price' => fake()->unique()->randomFloat(2, 5, 100),
+        ];
+    }
+}
